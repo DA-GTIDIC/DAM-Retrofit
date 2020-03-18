01319942cd161b20d5c76556f9e6118db31173c3
Jordi Mateo <jmateo@diei.udl.cat>
2020-03-18 20:08:35 +0100

    Example to see how to make a proper register to the custom API, passing the password encrypted using SHA256 and passlib format.

--- app/src/main/java/cat/udl/tidic/amd/dam_retrofit/MainActivity.java
+++ app/src/main/java/cat/udl/tidic/amd/dam_retrofit/MainActivity.java
@@ -10,13 +10,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.JsonObject;
+
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 import cat.udl.tidic.amd.dam_retrofit.models.UserModel;
 import cat.udl.tidic.amd.dam_retrofit.network.RetrofitClientInstance;
 import cat.udl.tidic.amd.dam_retrofit.services.UserService;
+import cat.udl.tidic.amd.dam_retrofit.utils.Utils;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -26,7 +30,9 @@ public class MainActivity extends AppCompatActivity {
     UserModel u = new UserModel();
     EditText tokenEditText;
     Button checkButton;
+    Button buttonRegister;
     TextView resultTextView;
+    UserService userService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +42,54 @@ public class MainActivity extends AppCompatActivity {
         tokenEditText = findViewById(R.id.editText_token);
         checkButton = findViewById(R.id.button_check);
         resultTextView = findViewById(R.id.textView_result);
+        buttonRegister = findViewById(R.id.button_register);
+
+        userService = RetrofitClientInstance.
+                getRetrofitInstance().create(UserService.class);
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Course API requires passwords in sha-256 in passlib format so:
+                String p = "1234";
+                String salt = "16";
+                String encode_hash = Utils.encode(p,salt,29000);
+                System.out.println("PASSWORD_ENCRYPTED " + encode_hash);
+
+
+                JsonObject user_json = new JsonObject();
+                user_json.addProperty("username", "jordimateo");
+                user_json.addProperty("name", "jordi");
+                user_json.addProperty("surname", "mateo");
+                user_json.addProperty("email", "a@gmail.com");
+                user_json.addProperty("phone", "972452389");
+                user_json.addProperty("genere", "M");
+                user_json.addProperty("password", encode_hash);
+
+                Call<Void> call = userService.registerUser(user_json);
+                call.enqueue(new Callback<Void>() {
+                    @Override
+                    public void onResponse(Call<Void> call, Response<Void> response) {
+                        if (response.code() == 200){
+                            Toast.makeText(MainActivity.this,"User registered", Toast.LENGTH_SHORT).show();
+                        }else{
+                            try {
+                                Toast.makeText(MainActivity.this, Objects.requireNonNull(response.errorBody()).string(), Toast.LENGTH_SHORT).show();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Void> call, Throwable t) {
+
+                    }
+                });
+
+            }
+        });
 
 
         checkButton.setOnClickListener(new View.OnClickListener() {
@@ -45,9 +99,6 @@ public class MainActivity extends AppCompatActivity {
 
                 Map<String, String> map = new HashMap<>();
                 map.put("Authorization", token);
-
-                UserService userService = RetrofitClientInstance.
-                        getRetrofitInstance().create(UserService.class);
 
                 Call<UserModel> call = userService.getUserProfile(map);
                 call.enqueue(new Callback<UserModel>() {
